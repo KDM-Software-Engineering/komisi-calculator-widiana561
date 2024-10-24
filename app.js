6ee8c2ffe835f4925fca437f6efb1fc810bceaa0
widiana561 <widianaagustiani@gmail.com>
2024-10-24 13:57:07 +0700

    selesai

--- app.js
+++ app.js
@@ -10,7 +10,20 @@
 
 // TULIS SCRIPT KAMU DI SINI
 
+function calculateKomisi(nilaiKontrak) {
+  if (nilaiKontrak >= 5000000){
+
+    return nilaiKontrak * 0.05;
+
+  } else if (nilaiKontrak >= 1000000) {
+    return 100000;
+
+  } else {
+    return 0;
+  }
+}
+console.log(calculateKomisi(5000000))
 
 
 // JANGAN HAPUS / UBAH SCRIPT DI BAWAH
-module.exports = calculateKomisi
+module.exports = calculateKomisi 
